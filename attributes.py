6b2f0542a8c36c388ef4e0525566be40db2935b0
lxrun0x <quietsato@gmail.com>
2018-06-25 09:54:13 +0900

    define classes and methods

---
+++ attributes.py
@@ -0,0 +1,19 @@
+class Attributes():
+    pass
+
+
+class Condition(Attributes):
+    def __init__(self):
+        # TODO set default value
+        pass
+
+
+class Data(Attributes):
+    name = ''
+    address = ''
+    url = ''
+    imageUrl = ''
+
+    def __init___(self, json):
+        # TODO set restaurant data from json
+        pass
