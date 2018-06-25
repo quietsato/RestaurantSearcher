25074aea047d53b70646bec99b6edb8c4755d0a0
lxrun0x <quietsato@gmail.com>
2018-06-25 16:16:23 +0900

    add urls setting file

---
+++ urls.py
@@ -0,0 +1,7 @@
+# h_apiurl : Hot Pepper API request url
+# g_apiurl : Google Maps Geocoding API request url
+# g_search : Google Search URL (.../search?q=SEARCH_WORD)
+
+h_apiurl = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
+g_apiurl = 'https://maps.googleapis.com/maps/api/geocode/json/'
+g_search = 'https://www.google.co.jp/search'
