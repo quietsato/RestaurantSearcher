cb507e44e17146c6bdc0e19f2d7c5d5a748d2d43
lxrun0x <quietsato@gmail.com>
2018-06-26 21:18:56 +0900

    アプリケーション開始処理を編集

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import window
+# import sys
+#
+# args = sys.argv
+# オプションを指定して起動するとクイック検索をすることができる
+# [使用例]
+#  python3 main.py [Option] value...
+#
+# オプション -c cityname
+#           -l lat lon
+#
 
-print('Hello')
+def startNormal():
+    window.Search()
+
+startNormal()
