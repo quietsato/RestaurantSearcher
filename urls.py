78d7e22e9cb01184b4cefca5fdd91d7bfe336626
lxrun0x <quietsato@gmail.com>
2018-06-26 21:05:31 +0900

    edit url

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@
 # g_apiurl : Google Maps Geocoding API request url
 # g_search : Google Search URL (.../search?q=SEARCH_WORD)
 
-h_apiurl = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
-g_apiurl = 'https://maps.googleapis.com/maps/api/geocode/json/'
+h_apiurl = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1'
+g_apiurl = 'https://maps.googleapis.com/maps/api/geocode/json'
 g_search = 'https://www.google.co.jp/search'
