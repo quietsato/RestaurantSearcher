fa8146db1b1b9d97e901cf15b0f9262c30a26d20
lxrun0x <quietsato@gmail.com>
2018-07-06 17:49:46 +0900

    フィールドを辞書型に変更

--- attributes.py
+++ attributes.py
@@ -1,49 +1,50 @@
 class Attributes:
-    genre_name = ''
-    close_day = ''
-    wifi = ''
-    free_drink = ''
-    free_food = ''
-    private_room = ''
-    non_smoking = ''
-    charter = ''
-    parking = ''
-    pass
+    data = {'genre_name': '',
+            'close_day': '',
+            'wifi': '',
+            'free_drink': '',
+            'free_food': '',
+            'private_room': '',
+            'non_smoking': '',
+            'charter': '',
+            'parking': ''
+            }
 
 
 class Condition(Attributes):
-    # このクラスはAttributesのフィールドをそのまま持っている
-    pass
+    def __init__(self):
+        for key in self.data.keys():
+            self.data[key] = '指定しない'
 
 
 class Data(Attributes):
-    name = ''
-    address = ''
-    url = ''
-    imageUrl = ''
-    other_data = {
-        'catch': '',
-        'open': '',
-        'course': '',
-        'midnight': ''
-    }
 
     def __init__(self, r_data):
-        self.name = r_data['name']
-        self.address = r_data['address']
-        self.url = r_data['urls']['pc']
-        self.imageUrl = r_data['photo']['pc']['l']
+        additional = {'name': '',
+                      'address': '',
+                      'url': '',
+                      'imageUrl': '',
+                      'catch': '',
+                      'open': '',
+                      'course': '',
+                      'midnight': ''}
+        self.data.update(additional)
 
-        self.genre_name = r_data['genre']['name']
-        self.charter = r_data['charter']
-        self.close_day = r_data['close']
-        self.free_drink = r_data['free_drink']
-        self.free_food = r_data['free_food']
-        self.wifi = r_data['wifi']
-        self.private_room = r_data['private_room']
-        self.non_smoking = r_data['non_smoking']
+        self.data['name'] = r_data['name']
+        self.data['address'] = r_data['address']
+        self.data['url'] = r_data['urls']['pc']
+        self.data['imageUrl'] = r_data['photo']['pc']['l']
 
-        self.other_data['catch'] = r_data['catch']
-        self.other_data['open'] = r_data['open']
-        self.other_data['course'] = r_data['course']
-        self.other_data['midnight'] = r_data['midnight']
+        self.data['genre_name'] = r_data['genre']['name']
+        self.data['charter'] = r_data['charter']
+        self.data['close_day'] = r_data['close']
+        self.data['free_drink'] = r_data['free_drink']
+        self.data['free_food'] = r_data['free_food']
+        self.data['wifi'] = r_data['wifi']
+        self.data['private_room'] = r_data['private_room']
+        self.data['non_smoking'] = r_data['non_smoking']
+
+        self.data['catch'] = r_data['catch']
+        self.data['open'] = r_data['open']
+        self.data['course'] = r_data['course']
+        self.data['midnight'] = r_data['midnight']
