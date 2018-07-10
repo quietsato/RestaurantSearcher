25dc90a51936cb82a2505b93a089f1b1fd198ff5
lxrun0x <quietsato@gmail.com>
2018-07-10 13:07:13 +0900

    ConditionやDataの初期値の渡し方を変更

--- attributes.py
+++ attributes.py
@@ -1,16 +1,7 @@
 # このファイルには変数定義のみが書かれている
 # ここの変数の値は変更しないこと
 
-default = '指定しない'
-condition = {'genre_name': default,
-             'wifi': default,
-             'free_drink': default,
-             'free_food': default,
-             'private_room': default,
-             'non_smoking': default,
-             'charter': default,
-             'parking': default
-             }
+condition_default = '指定しない'
 condition_keys = [
     'genre_name', 'wifi', 'free_drink', 'free_food',
     'private_room', 'non_smoking', 'charter', 'parking'
@@ -36,8 +27,40 @@ data = {'name': '',
         'course': '',
         'midnight': '',
         'close_day': ''}
-data.update(condition)
+data_keys = ['name', 'address', 'url', 'imageUrl', 'catch',
+             'open', 'course', 'midnight', 'close_day'] + condition_keys
 
-for key in data.keys():
-    data[key] = ''
+def getCondition():
+    # 戻り地のテンプレートの作成
+    cond = {
+        key: condition_default for key in condition_keys
+    }
 
+    return cond
+
+def convertData(res=None):
+    # 戻り値のテンプレートの作成
+    r_data = {
+        key: '' for key in data_keys
+    }
+
+    r_data['name'] = res['name']
+    r_data['address'] = res['address']
+    r_data['url'] = res['urls']['pc']
+    r_data['imageUrl'] = res['photo']['pc']['l']
+
+    r_data['genre_name'] = res['genre']['name']
+    r_data['charter'] = res['charter']
+    r_data['close_day'] = res['close']
+    r_data['free_drink'] = res['free_drink']
+    r_data['free_food'] = res['free_food']
+    r_data['wifi'] = res['wifi']
+    r_data['private_room'] = res['private_room']
+    r_data['non_smoking'] = res['non_smoking']
+
+    r_data['catch'] = res['catch']
+    r_data['open'] = res['open']
+    r_data['course'] = res['course']
+    r_data['midnight'] = res['midnight']
+
+    return r_data
